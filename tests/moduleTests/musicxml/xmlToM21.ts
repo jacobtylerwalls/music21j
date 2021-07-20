c1fa020ff0b518420b85462174a2250f0eec6b1a
Jacob Walls <jacobtylerwalls@gmail.com>
2021-07-20 10:04:56 -0400

    point to my fork

--- tests/moduleTests/musicxml/xmlToM21.ts
+++ tests/moduleTests/musicxml/xmlToM21.ts
@@ -6,7 +6,7 @@ const { test } = QUnit;
 
 export default function tests() {
     test('music21.musicxml.xmlToM21 clef stored on part', assert => {
-        const mxUrl = '../../../testHTML/credo_scabroso_excerpt.musicxml';
+        const mxUrl = 'https://raw.githubusercontent.com/jacobtylerwalls/music21j/c213b1bf125180fdc5deb71126fb86b3849ecc60/testHTML/credo_scabroso_excerpt.musicxml';
         const sp = new music21.musicxml.xmlToM21.ScoreParser();
         sp.scoreFromUrl(mxUrl);
         const bass = sp.stream.parts[2];
