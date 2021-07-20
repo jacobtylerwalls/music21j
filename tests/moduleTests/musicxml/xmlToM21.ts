df2674379aa84c7ffd862c831d66c28a11f3760f
Jacob Walls <jacobtylerwalls@gmail.com>
2021-07-20 10:07:39 -0400

    wait?

--- tests/moduleTests/musicxml/xmlToM21.ts
+++ tests/moduleTests/musicxml/xmlToM21.ts
@@ -8,7 +8,7 @@ export default function tests() {
     test('music21.musicxml.xmlToM21 clef stored on part', assert => {
         const mxUrl = 'https://raw.githubusercontent.com/jacobtylerwalls/music21j/c213b1bf125180fdc5deb71126fb86b3849ecc60/testHTML/credo_scabroso_excerpt.musicxml';
         const sp = new music21.musicxml.xmlToM21.ScoreParser();
-        sp.scoreFromUrl(mxUrl);
+        const unused = sp.scoreFromUrl(mxUrl);
         const bass = sp.stream.parts[2];
         assert.ok(bass.clef.isClassOrSubclass('BassClef'), 'clef in bass part is BassClef');
     });
